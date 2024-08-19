02d208591f9df59e606c7e8a70e8db0bb5da51f6
Rohini Patil <rohine452001@gmail.com>
2024-08-19 21:01:00 +0530

    Adding movies

---
+++ src/main/java/com/acciojob/book_my_show_august/Models/Movie.java
@@ -0,0 +1,35 @@
+package com.acciojob.book_my_show_august.Models;
+
+import com.acciojob.book_my_show_august.Enums.Language;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name =  "movies")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer movieId;
+
+    @Column(unique = true)
+    private String movieName;
+
+    private Double duration;
+
+    private LocalDate releaseDate;
+
+    @Enumerated(value = EnumType.STRING)
+    private Language language;
+
+    private Double rating;
+
+}
