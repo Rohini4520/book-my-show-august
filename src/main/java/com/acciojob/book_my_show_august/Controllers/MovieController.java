a01d3d8550e540c4302ccf4b5f7b3a0f09f1ade4
Rohini <rohine452001@gmail.com>
2024-08-19 21:16:40 +0530

    Adding movie and theater

---
+++ src/main/java/com/acciojob/book_my_show_august/Controllers/MovieController.java
@@ -0,0 +1,30 @@
+package com.acciojob.book_my_show_august.Controllers;
+
+
+import com.acciojob.book_my_show_august.Requests.AddMovieRequest;
+import com.acciojob.book_my_show_august.Requests.UpdateMovieRequest;
+import com.acciojob.book_my_show_august.Service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("movie")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @PostMapping("add")
+    public ResponseEntity addMovie(@RequestBody AddMovieRequest movieRequest){
+
+        String response = movieService.addMovie(movieRequest);
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+    @PutMapping("update")
+    public ResponseEntity updateMovieAttributes(@RequestBody UpdateMovieRequest updateMovieRequest){
+
+        String response = movieService.updateMovieAttributes(updateMovieRequest);
+        return  new ResponseEntity(response, HttpStatus.OK);
+    }
+
+}
