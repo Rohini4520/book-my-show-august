13da2359fa95335a9428199d25de1c687a362f16
Rohini Patil <rohine452001@gmail.com>
2024-09-30 06:35:34 +0530

    Added BookMyShowAugustApplication file

---
+++ src/main/java/com/acciojob/book_my_show_august/BookMyShowAugustApplication.java
@@ -0,0 +1,13 @@
+package com.acciojob.book_my_show_august;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowAugustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowAugustApplication.class, args);
+	}
+
+}
